039907c42fc55d8832d75141bd7855c9a17506aa
biletweb-macmini <bilet.igor@gmail.com>
2025-01-03 22:58:22 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <span class="text-3xl font-bold underline">Product filter</span>
+  </header>
+
+  <RouterView />
+</template>
