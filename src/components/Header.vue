aaba6ca0b6369904e5717a73a5e3db6feaef68f6
biletweb-macmini <bilet.igor@gmail.com>
2025-01-04 22:00:59 +0200

    fix

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="p-4 bg-slate-300">
+  <header class="rounded-b-lg p-4 bg-slate-300">
     <span class="text-3xl font-bold">Product Filter</span>
   </header>
 </template>
