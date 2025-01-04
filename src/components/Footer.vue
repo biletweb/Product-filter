c96bde51ffdeb3d262d26b5839906089c436f0ac
biletweb-macmini <bilet.igor@gmail.com>
2025-01-04 23:20:09 +0200

    fix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
-  <footer class="rounded-t-lg bg-slate-300 py-4">
+  <footer class="rounded-t-lg bg-slate-300 text-sky-500 py-4">
     <div class="text-center text-lg">
       © 2024-{{ currentYear }} biletweb.
-      <a href="#" target="_blank" class="hover:text-slate-500 hover:underline">License MIT</a>. Version: {{ appVersion }}
+      <a href="#" target="_blank" class="hover:text-sky-600 hover:underline">License MIT</a>. Version: {{ appVersion }}
     </div>
   </footer>
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="rounded-b-lg p-4 bg-slate-300">
     <div class="flex items-center justify-between">
-    <span class="text-3xl font-bold">Product Filter</span>
+    <span class="text-3xl font-bold text-sky-500">Product Filter</span>
     <div>
       <router-link :to="{ name: 'home' }" class="mr-4">Home</router-link>
     </div>
