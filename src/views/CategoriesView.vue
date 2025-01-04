292d3ee8644e728e12ce6561dabd8b81a50198a6
biletweb-macmini <bilet.igor@gmail.com>
2025-01-04 21:34:56 +0200

    fix

--- src/views/CategoriesView.vue
+++ src/views/CategoriesView.vue
@@ -1,3 +1,10 @@
 <template>
-    <span class="text-3xl font-bold underline">Categories</span>
+  <div class="text-3xl mb-4 font-bold">Categories</div>
+  <div class="grid grid-cols-5 gap-4">
+    <div>01</div>
+    <div>02</div>
+    <div>03</div>
+    <div>04</div>
+    <div>05</div>
+  </div>
 </template>
