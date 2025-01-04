41536c25acf62659f8392b256895623fed211821
biletweb-macmini <bilet.igor@gmail.com>
2025-01-04 21:56:11 +0200

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import CategoriesView from '../views/CategoriesView.vue'
+import SubcategoriesView from '../views/SubcategoriesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: CategoriesView,
+    },
+    {
+      path: '/subcategory/:id',
+      name: 'subcategory',
+      component: SubcategoriesView,
     },
   ],
 })
