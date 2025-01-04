9e6c9bd0b663004cda39ffcd54847b0d7cfb32ab
biletweb-macmini <bilet.igor@gmail.com>
2025-01-04 21:45:17 +0200

    fix

--- src/views/CategoriesView.vue
+++ src/views/CategoriesView.vue
@@ -1,10 +1,30 @@
 <template>
   <div class="text-3xl mb-4 font-bold">Categories</div>
   <div class="grid grid-cols-5 gap-4">
-    <div>01</div>
-    <div>02</div>
-    <div>03</div>
-    <div>04</div>
-    <div>05</div>
+    <div v-for="category in categories" :key="category.id">
+      <router-link :to="{ name: 'category', params: { id: category.id } }">
+        {{ category.name }}
+      </router-link>
+    </div>
   </div>
 </template>
+
+<script setup>
+import axios from 'axios'
+import { ref, onMounted } from 'vue'
+
+onMounted(() => {
+  getCategories()
+})
+
+const categories = ref([])
+
+const getCategories = async () => {
+  const response = await axios.get('https://127.0.0.1:8000/api/categories', {
+    headers: {
+      Accept: 'application/json',
+    },
+  })
+  categories.value = response.data.categories
+}
+</script>
