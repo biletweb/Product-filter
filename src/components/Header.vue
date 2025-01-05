3ee310d804d60bb434d1715dffc40cfd1d43bdc2
biletweb-macmini <bilet.igor@gmail.com>
2025-01-05 14:38:55 +0200

    fix

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
     <div class="flex items-center justify-between">
     <span class="text-3xl font-bold text-sky-500">Product Filter</span>
     <div>
-      <router-link :to="{ name: 'home' }" class="mr-4">Home</router-link>
+      <router-link :to="{ name: 'home' }" class="text-sky-500 hover:text-sky-600">Home</router-link>
     </div>
   </div>
   </header>
